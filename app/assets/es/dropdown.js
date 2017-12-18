0dfe97842a1f82fe0009d53c659dd5511fc07238
stijstev <stijstev@student.arteveldehs.be>
2017-12-18 13:14:33 +0100

    Fix navbar

--- app/assets/es/dropdown.js
+++ app/assets/es/dropdown.js
@@ -1,26 +1,35 @@
 'use strict';
-let dropdownmenu = document.querySelector('#drop-menu');
+let dropDownMenu = document.querySelector('#drop-menu');
 let hamburger = document.querySelector('#hamburger-menu');
+let menuActive = false;
 
 export class DropDown {
   constructor () {
     console.log('this is the dropdown');
   }
-
   createDropDown () {
     hamburger.addEventListener('click', function () {
       console.log('open');
-      dropdownmenu.setAttribute('style', 'display: block');
-      hamburger.src = '/assets/images/symbols/cross_white.svg';
-      hamburger.classList.add('.btn-close');
-    });
-  }
-
-  closeDropDown () {
-    let cross = document.querySelector('.close_btn');
-    cross.addEventListener('click', function () {
-      console.log('close');
-      dropdownmenu.setAttribute('style', 'display: none');
+      if (!menuActive) {
+        dropDownMenu.setAttribute('style', 'display: block');
+        hamburger.src = '/assets/images/symbols/cross_white.svg';
+        hamburger.classList.add('.btn-close');
+        menuActive = true;
+      } else {
+        dropDownMenu.setAttribute('style', 'display: none');
+        hamburger.src = '/assets/images/symbols/menu.svg';
+        hamburger.classList.remove('.btn-close');
+        menuActive = false;
+      }
     });
   }
 }
+
+//   closeDropDown () {
+//     let cross = document.querySelector('.close_btn');
+//     cross.addEventListener('click', function () {
+//       console.log('close');
+//       dropdownmenu.setAttribute('style', 'display: none');
+//     });
+//   }
+// }
